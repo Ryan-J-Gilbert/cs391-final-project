0a8284c5563f62598620f16234e634fec2f56a16
sethculberson <sethculb@bu.edu>
2025-11-21 12:02:24 -0500

    Completed history layout

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -5,7 +5,25 @@
  * 
  * Page to display user's budgeting history.
  */
+"use client";
+import { useState } from "react";
+import { MonthEntry } from "@/types/MonthEntry";
 
-export default function History() {
-    return <div>History Page - To be implemented</div>;
+export default function History({monthEntries} : {monthEntries: MonthEntry[]}) {
+    const [entries, setEntries] = useState(monthEntries);
+
+    return (
+        <div className="">
+            <h2 className="">History</h2>
+            {entries.map((m : MonthEntry) => (
+                <div key={String(m.month)+"/"+String(m.year)} className="">
+                    <p className="">Date: {m.month+"/"+m.year}</p>
+                    <p className="">Wants: ${m.wants}</p>
+                    <p className="">Needs: ${m.needs}</p>
+                    <p className="">Savings: ${m.savings}</p>
+                    <p className="">Total Budget: ${m.total}</p>
+                </div>
+            ))}
+        </div>
+    );
 }
---
+++ types/MonthEntry.ts
@@ -0,0 +1,8 @@
+export type MonthEntry ={
+    month: number;
+    year: number;
+    wants: number;
+    needs: number;
+    savings: number;
+    total: number;
+}
