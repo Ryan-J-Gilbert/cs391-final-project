d1574335b917e906b10c57f5ddf62778887c0062
sethculberson <sethculb@bu.edu>
2025-11-21 11:45:14 -0500

    Added history directory and page

---
+++ app/history/page.tsx
@@ -0,0 +1,11 @@
+/*
+ * History Page
+ * Author: Seth Culberson
+ * Email: sethculb@bu.edu
+ * 
+ * Page to display user's budgeting history.
+ */
+
+export default function History() {
+    return <div>History Page - To be implemented</div>;
+}
