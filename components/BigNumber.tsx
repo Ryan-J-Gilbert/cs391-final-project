8feb7aa6fc2cf6f48e3c28872a4aa9b680ee8b84
Ryan Gilbert <ryanjg@bu.edu>
2025-12-05 12:18:45 -0500

    fix: adjust contrast and use border coloring

--- components/BigNumber.tsx
+++ components/BigNumber.tsx
@@ -4,15 +4,15 @@
 
 export default function BigNumber({ title, value, change }: { title: string, value: number, change: number }){
 
-    const color = change > 0 ? "text-green-500" : "text-red-500";
-    const changeClass = "text-xl " + color;
+    const color = change > 0 ? "border-green-500" : "border-red-500";
+    const changeClass = "border-3 w-32 aspect-square text-white bg-gray-600 rounded-lg flex flex-col items-center justify-center " + color;
     const changeText = (change > 0 ? "˄": "˅") + " $" + change;
 
     return (
-        <div className="w-32 aspect-square bg-blue-500 rounded-lg flex flex-col items-center justify-center">
+        <div className={changeClass}>
             <p className="text-2xl p-2">{title}</p>
             <p className="text-3xl p-2">${value}</p>
-            <p className={changeClass}>{changeText}</p>
+            <p className="text-xl">{changeText}</p>
         </div>
 
     )
